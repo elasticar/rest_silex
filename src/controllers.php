1317901b70b488a8623b59cb9a5575bbb82fe0f2
giedrskuzins <skuziokas@yahoo.com>
2017-04-27 15:01:41 +0300

    first commit

---
+++ src/controllers.php
@@ -0,0 +1,46 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig', array());
+})
+->bind('homepage')
+;
+
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/'.$code.'.html.twig',
+        'errors/'.substr($code, 0, 2).'x.html.twig',
+        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/default.html.twig',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
+
+$app->post('/api/needCarsByData', function (Request $request) use ($app) {
+
+    $data = json_decode($request->getContent(), true);
+
+    $sql = "SELECT * FROM elasticar.advertisement WHERE model LIKE ?";
+
+    $model=$data['model'];
+    $model="%".$model."%";
+
+    $advertisements = $app['db']->fetchAll($sql, array($model));
+
+    return $app->json($advertisements);
+
+});
